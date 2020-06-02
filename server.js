990bfd6ed14e8bbb7c5077e18878d1f6b2184f4f
Dheeraj Sharam <dj4385@gmail.com>
2020-06-02 18:42:58 -0400

    unusable fun removes

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 4400;
+const path = require('path');
+const cors = require('cors')
+app.use(cors());
+
+app.listen(port);
+
+app.use(express.static('dist/bitoco'));
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/*');
+// });
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/dist/bitoco/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'dist/bitoco')));
+
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+console.log("app is listing on port-->>",port);
