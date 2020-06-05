e37e5e203ba2685ce876afead91da0031a28ec9c
Dheeraj Sharam <dj4385@gmail.com>
2020-06-05 13:51:26 -0400

    routing removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxSpinnerModule } from "ngx-spinner";
@@ -14,13 +12,11 @@ import { TopHeadlineListComponent } from './top-headline-list/top-headline-list.
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     ErrorComponent,
     TopHeadlineListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,
     MaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
